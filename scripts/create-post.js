7b2aafd6c03bbe09ad76fb416a604f24ee5e8aa9
Sharqy <pencilmagicart@gmail.com>
2023-08-14 20:42:28 +0300

    finished create-post

--- scripts/create-post.js
+++ scripts/create-post.js
@@ -4,6 +4,7 @@ let createForm = document.querySelector("#create-post");
 createBtn.forEach((button)=>{
     button.addEventListener("click", ()=> {
     console.log("Button clicked");
+    window.scrollTo({top: 0, left: 0, behavior: "smooth"});
     createForm.style.boxShadow = "0px 0px 5px 3px var(--primary-color)";
     setTimeout(()=>{
         createForm.style.boxShadow = "none";
