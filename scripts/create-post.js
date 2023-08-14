c658e6954b8433491745afbe4d4ec39be3a1df38
Sharqy <pencilmagicart@gmail.com>
2023-08-14 21:20:03 +0300

    fixed search on small screens

--- scripts/create-post.js
+++ scripts/create-post.js
@@ -1,13 +1,15 @@
 let createBtn = document.querySelectorAll(".create");
 let createForm = document.querySelector("#create-post");
+let postInput = document.querySelector("#post-input");
 
 createBtn.forEach((button)=>{
     button.addEventListener("click", ()=> {
     console.log("Button clicked");
     window.scrollTo({top: 0, left: 0, behavior: "smooth"});
+    postInput.focus();
     createForm.style.boxShadow = "0px 0px 5px 3px var(--primary-color)";
     setTimeout(()=>{
         createForm.style.boxShadow = "none";
     }, 2000);
-})
+    })
 })
