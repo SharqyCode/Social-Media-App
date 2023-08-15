de8f4f5a28452e7b88642bcec1d838d32dd83f6a
Sharqy <pencilmagicart@gmail.com>
2023-08-15 23:37:00 +0300

    Added smooth scrolling

--- scripts/create-post.js
+++ scripts/create-post.js
@@ -5,7 +5,7 @@ let postInput = document.querySelector("#post-input");
 createBtn.forEach((button)=>{
     button.addEventListener("click", ()=> {
     console.log("Button clicked");
-    window.scrollTo({top: 0, left: 0, behavior: "smooth"});
+    window.scrollTo({top: 0, ScrollBehavior: 'smooth'});
     postInput.focus();
     createForm.style.boxShadow = "0px 0px 5px 3px var(--primary-color)";
     setTimeout(()=>{
