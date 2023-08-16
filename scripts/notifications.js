4f9d6a3f70af952a7c6cefd4c8203492ae9f3b60
Sharqy <pencilmagicart@gmail.com>
2023-08-16 17:27:20 +0300

    Added notifications.js

--- scripts/notifications.js
+++ scripts/notifications.js
@@ -1,6 +1,38 @@
 let notifLink = document.querySelector("#notif-link");
+let navLinks = document.querySelectorAll(".navLink");
 
 notifLink.addEventListener("click", (e)=>{
     e.preventDefault();
-    console.log("notif");
+    
+    navLinks.forEach((link)=>{
+        link.classList.remove("active");
+    })
+    notifLink.classList.add("active");
+
+    let notifList = document.createElement("ul");
+    notifList.classList.add("notif-list");
+
+    let notifListItem = document.createElement("li");
+
+    let notif = document.createElement("a");
+    notif.classList.add("notif");
+
+    let notifPfp = document.createElement("img");
+    notifPfp.classList.add("notif__pfp", "pfp");
+
+    let notifInfo = document.createElement("div");
+    notifInfo.classList.add("notif__info");
+
+    let notifText = document.createElement("p");
+    notifText.classList.add("notif__text");
+
+    let notifTextUser = document.createElement("span");
+    notifTextUser.classList.add("notif__text_user");
+
+    let notifTime =  document.createElement("span");
+    notifTime.classList.add("notif__time");
+
+
+    
+
 })
