cfb995c3188527de6e1c6be3728f944074b48449
Sharqy <pencilmagicart@gmail.com>
2023-08-21 01:00:58 +0300

    Fixed stories.js

--- scripts/stories.js
+++ scripts/stories.js
@@ -1,13 +1,17 @@
 let stories = document.querySelectorAll(".stories-card");
 let storyBG = document.createElement("div");
 storyBG.classList.add("storyBG");
+storyBG.style.zIndex = "11";
 let storyLarge = document.createElement("div");
 storyLarge.classList.add("storyLarge");
 
 stories.forEach((story)=>{
     story.addEventListener("click", ()=>{
+        let storyLarge = document.createElement("div");
+        storyLarge.classList.add("storyLarge");
         storyLarge.style.backgroundImage = window.getComputedStyle(story).backgroundImage;
-        let storyLargePfp = story.querySelector("img");
+        let storyLargePfp = document.createElement("img");
+        storyLargePfp.src = story.querySelector("img").src;
         storyLargePfp.classList.remove("stories-card__profile");
         storyLargePfp.classList.add("storyLarge__pfp", "pfp");
         storyLarge.appendChild(storyLargePfp);
@@ -19,6 +23,6 @@ stories.forEach((story)=>{
 
 storyBG.addEventListener("click", ()=>{
     document.body.style.overflowY = "scroll";
-    storyBG.remove(storyLarge);
+    // storyBG.remove(storyLarge);
     document.body.removeChild(storyBG);
 })
