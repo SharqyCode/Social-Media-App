f5babff5220ae3604f562a02b13ea07107e1359d
Sharqy <pencilmagicart@gmail.com>
2023-08-23 18:07:06 +0300

    Finally done (image fix css bgimage)

--- scripts/theme.js
+++ scripts/theme.js
@@ -61,111 +61,119 @@ cardBg.appendChild(themeCard);
 let root = document.querySelector(":root");
 
 // Set theme according to local storage values
-document.addEventListener("DOMContentLoaded", ()=>{
-    if (window.localStorage.getItem("font") != null)
-    {
+document.addEventListener("DOMContentLoaded", () => {
+    if (window.localStorage.getItem("font") != null) {
         root.style.setProperty("font-size",
-        `${window.localStorage.getItem("font")}px`);
+            `${window.localStorage.getItem("font")}px`);
+    }
+    else {
+        window.localStorage.setItem("font", "16");
     }
 
-    if (window.localStorage.getItem("color") != null)
-    {
+    if (window.localStorage.getItem("color") != null) {
         root.style.setProperty("--primary-color",
-        `var(--color-${window.localStorage.getItem("color")})`);
+            `var(--color-${window.localStorage.getItem("color")})`);
+    }
+    else {
+        window.localStorage.setItem("color", "purple");
     }
 
-    if (window.localStorage.getItem("bg") != null)
-        {
+
+    if (window.localStorage.getItem("bg") != null) {
         root.style.setProperty("--cur-bg",
-        `var(--${window.localStorage.getItem("bg")}-bg)`);
+            `var(--${window.localStorage.getItem("bg")}-bg)`);
+    }
+    else {
+        window.localStorage.setItem("bg", "light");
     }
 
-    if (window.localStorage.getItem("widget") != null)
-    {
+    if (window.localStorage.getItem("widget") != null) {
         root.style.setProperty("--cur-widget",
-        `var(--${window.localStorage.getItem("widget")}-widget)`);
+            `var(--${window.localStorage.getItem("widget")}-widget)`);
+    }
+    else {
+        window.localStorage.setItem("widget", "light");
     }
 
-    if (window.localStorage.getItem("primaryFont") != null)
-    {
+    if (window.localStorage.getItem("primaryFont") != null) {
         root.style.setProperty("--primary-font",
-        `var(--${window.localStorage.getItem("primaryFont")}-font)`);
+            `var(--${window.localStorage.getItem("primaryFont")}-font)`);
+    }
+    else {
+        window.localStorage.setItem("primaryFont", "light");
     }
 
-    if (window.localStorage.getItem("secondaryFont") != null)
-    {
+    if (window.localStorage.getItem("secondaryFont") != null) {
         root.style.setProperty("--secondary-font",
-        `var(--${window.localStorage.getItem("secondaryFont")}-font)`);
+            `var(--${window.localStorage.getItem("secondaryFont")}-font)`);
+    }
+    else {
+        window.localStorage.setItem("secondaryFont", "dark");
     }
 })
 
 // Open theme card on click
-themeLink.addEventListener("click", ()=>{
+themeLink.addEventListener("click", () => {
     document.body.appendChild(cardBg);
     let sizes = document.querySelectorAll(".size-selector__size");
     let colors = document.querySelectorAll(".color-selector__color");
     let backgrounds = document.querySelectorAll(".background-selector__background");
-    
+
     // Set active elements according to local storage values
-    sizes.forEach((size)=>{
+    sizes.forEach((size) => {
         size.classList.remove("active");
     })
     document
-    .querySelector(`#f${window.localStorage.getItem("font")}`)
-    .classList.add("active");
-    
-    colors.forEach((color)=>{
+        .querySelector(`#f${window.localStorage.getItem("font")}`)
+        .classList.add("active");
+
+    colors.forEach((color) => {
         color.classList.remove("active");
     })
     document
-    .querySelector(`#${window.localStorage.getItem("color")}`)
-    .classList.add("active");
-    
-    backgrounds.forEach((background)=>{
+        .querySelector(`#${window.localStorage.getItem("color")}`)
+        .classList.add("active");
+
+    backgrounds.forEach((background) => {
         background.classList.remove("active");
     })
     document
-    .querySelector(`#${window.localStorage.getItem("bg")}`)
-    .classList.add("active");
-    
+        .querySelector(`#${window.localStorage.getItem("bg")}`)
+        .classList.add("active");
+
     // Add open class to signify addition/removal of card
     themeCard.classList.add("open")
     let closeTheme = document.querySelector(".theme-card__close");
-    closeTheme.addEventListener("click", ()=>{
+    closeTheme.addEventListener("click", () => {
         themeCard.classList.remove("open")
         document.body.removeChild(cardBg);
     })
-    
+
     // Choose font size and add to local storage
     let fontSize;
-    sizes.forEach((size)=>{
-        size.addEventListener("click", ()=>{
-            if (!size.classList.contains("active"))
-            {
-                if (size.classList.contains("x-small"))
-                {
+    sizes.forEach((size) => {
+        size.addEventListener("click", () => {
+            if (!size.classList.contains("active")) {
+                if (size.classList.contains("x-small")) {
                     fontSize = 12;
                 }
-                if (size.classList.contains("small"))
-                {
-                    fontSize = 14;}
-                if (size.classList.contains("normal"))
-                {
+                if (size.classList.contains("small")) {
+                    fontSize = 14;
+                }
+                if (size.classList.contains("normal")) {
                     fontSize = 16;
                 }
-                if (size.classList.contains("large"))
-                {
+                if (size.classList.contains("large")) {
                     fontSize = 18;
                 }
-                if (size.classList.contains("x-large"))
-                {
+                if (size.classList.contains("x-large")) {
                     fontSize = 20;
                 }
-                
+
                 root.style.setProperty("font-size",
-                `${fontSize}px`);
+                    `${fontSize}px`);
                 window.localStorage.setItem("font", fontSize);
-                sizes.forEach((size)=>{
+                sizes.forEach((size) => {
                     size.classList.remove("active");
                 })
                 size.classList.add("active");
@@ -175,15 +183,14 @@ themeLink.addEventListener("click", ()=>{
 
     // Choose primary color and add to local storage
     let primaryColor;
-    colors.forEach((color)=>{
-        color.addEventListener("click", ()=>{
-            if (!color.classList.contains("active"))
-            {
-                primaryColor = color.classList[color.classList.length-1];
+    colors.forEach((color) => {
+        color.addEventListener("click", () => {
+            if (!color.classList.contains("active")) {
+                primaryColor = color.classList[color.classList.length - 1];
                 window.localStorage.setItem("color", primaryColor);
                 root.style.setProperty("--primary-color",
-                `var(--color-${primaryColor})`);
-                colors.forEach((color)=>{
+                    `var(--color-${primaryColor})`);
+                colors.forEach((color) => {
                     color.classList.remove("active");
                 })
                 color.classList.add("active");
@@ -192,25 +199,24 @@ themeLink.addEventListener("click", ()=>{
     })
 
 
-     // Choose bg, widget, and font colors and add to local storage
+    // Choose bg, widget, and font colors and add to local storage
     let widgetColor;
     let bgColor;
     let primaryFont;
     let secondaryFont;
-    backgrounds.forEach((background)=>{
-        background.addEventListener("click", ()=>{
-            if (!background.classList.contains("active"))
-            {
-                bgColor = background.classList[background.classList.length-1];
+    backgrounds.forEach((background) => {
+        background.addEventListener("click", () => {
+            if (!background.classList.contains("active")) {
+                bgColor = background.classList[background.classList.length - 1];
                 root.style.setProperty("--cur-bg",
-                `var(--${bgColor}-bg)`);
+                    `var(--${bgColor}-bg)`);
                 window.localStorage.setItem("bg", bgColor);
 
-                widgetColor = background.classList[background.classList.length-1];
+                widgetColor = background.classList[background.classList.length - 1];
                 root.style.setProperty("--cur-widget",
-                `var(--${widgetColor}-widget)`);
+                    `var(--${widgetColor}-widget)`);
                 window.localStorage.setItem("widget", widgetColor);
-                
+
                 if (!background.classList.contains("light")) {
                     primaryFont = "dark";
                     secondaryFont = "light";
@@ -221,13 +227,13 @@ themeLink.addEventListener("click", ()=>{
                 }
 
                 root.style.setProperty("--primary-font",
-                `var(--${primaryFont}-font)`);
+                    `var(--${primaryFont}-font)`);
                 window.localStorage.setItem("primaryFont", primaryFont);
                 root.style.setProperty("--secondary-font",
-                `var(--${secondaryFont}-font)`);
+                    `var(--${secondaryFont}-font)`);
                 window.localStorage.setItem("secondaryFont", secondaryFont);
 
-                backgrounds.forEach((background)=>{
+                backgrounds.forEach((background) => {
                     background.classList.remove("active");
                 })
                 background.classList.add("active");
@@ -237,9 +243,8 @@ themeLink.addEventListener("click", ()=>{
 })
 
 // Close theme card when click outside
-document.addEventListener("click", (e)=>{
-    if (e.target.classList.contains("card-bg"))
-    {
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("card-bg")) {
         themeCard.classList.remove("open")
         document.body.removeChild(cardBg);
         themeLink.classList.remove("active");
